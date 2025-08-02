cc4de234eeaa1444bd02b2d83e12a08e742e9a25
staratip <taratip.s@chula.ac.th>
2025-08-02 19:59:03 +0700

    Add more test cases for input validation

--- triangle.test.js
+++ triangle.test.js
@@ -15,3 +15,16 @@ test('Scalene triangle', () => {
 test('Not a triangle', () => {
   expect(triangleType(1, 2, 3)).toBe("NotATriangle");
 });
+
+test('Invalid input: 0, 5, 5', () => {
+  expect(triangleType(0, 5, 5)).toBe("InvalidInput");
+});
+
+test('Invalid input: 201, 5, 5', () => {
+  expect(triangleType(201, 5, 5)).toBe("InvalidInput");
+});
+
+test('Invalid input: "a", 5, 5', () => {
+  expect(triangleType("a", 5, 5)).toBe("InvalidInput");
+});
+
